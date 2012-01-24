a531214bee62243be14360c98af2c571f985bbd5
Paul Whitrow <paul@pwhitrow.com>
2012-01-24 20:32:10 +0000

    updated socials plugin

--- api/plugins/socials/jquery.socials.js
+++ api/plugins/socials/jquery.socials.js
@@ -24,13 +24,13 @@
             }, options);
             
             settings.share = [
-            {media: 'Facebook', link: 'http://www.facebook.com/share.php?u=' + escape(settings.href) + '&amp;title=' + settings.title, 'img': 'facebook.png'},
-            {media: 'Twitter', link: 'http://www.twitter.com/home?status=' + escape(settings.href) + '&amp;status=' + escape(settings.href), 'img': 'twitter.png'},
-            {media: 'Delicious', link: 'http://del.icio.us/post?url=' + escape(settings.href) + '&amp;title=' + settings.title, 'img': 'delicious.png'},
-            {media: 'Digg', link: 'http://digg.com/submit?phase=2&amp;url=' + escape(settings.href) + '&amp;title=' + settings.title, 'img': 'digg.png'},
-            {media: 'Reddit', link: 'http://reddit.com/submit?url=' + escape(settings.href) + '&amp;title=' + settings.title, 'img': 'reddit.png'},
-            {media: 'NewsVine', link: 'http://www.newsvine.com/_tools/seed&amp;save?u=' + escape(settings.href) + '&amp;title=' + settings.title, 'img': 'newsvine.png'},
-            {media: 'StumbleUpon', link: 'http://www.stumbleupon.com/submit?url=' + escape(settings.href) + '&amp;title=' + settings.title, 'img': 'stumbleupon.png'}
+            {media: 'Facebook', link: 'http://www.facebook.com/share.php?u=' + escape(settings.href) + '&amp;title=' + settings.title},
+            {media: 'Twitter', link: 'http://www.twitter.com/home?status=' + escape(settings.href) + '&amp;status=' + escape(settings.href)},
+            {media: 'Delicious', link: 'http://del.icio.us/post?url=' + escape(settings.href) + '&amp;title=' + settings.title},
+            {media: 'Digg', link: 'http://digg.com/submit?phase=2&amp;url=' + escape(settings.href) + '&amp;title=' + settings.title},
+            {media: 'Reddit', link: 'http://reddit.com/submit?url=' + escape(settings.href) + '&amp;title=' + settings.title},
+            {media: 'NewsVine', link: 'http://www.newsvine.com/_tools/seed&amp;save?u=' + escape(settings.href) + '&amp;title=' + settings.title},
+            {media: 'StumbleUpon', link: 'http://www.stumbleupon.com/submit?url=' + escape(settings.href) + '&amp;title=' + settings.title}
             ];
 
             $('<ul />').attr(
@@ -42,46 +42,8 @@
             {
                 $('<li />').attr(
                 {
-                    'class': '_bf_social_bookmark_text'
-                })
-                .html(_bf.t('Share') + ':')
-                .appendTo($(this))
-                
-                $('<li />').attr(
-                {
                     'class': '_bf_social_bookmark _bf_social_bookmarks_likes'
-                })
-                .each(function()
-                {
-//                    $('<span />').attr(
-//                    {
-//                        'class': '_bf_social_bookmark'
-//                    })
-//                    .each(function()
-//                    {
-////                $(this).append('<script src="//platform.twitter.com/widgets.js" type="text/javascript"></script><div><a href="https://twitter.com/share?url=' + _bf.host + '" class="twitter-share-button">Tweet</a></div>');
-////                $(this).append("<span  class='st_linkedin_hcount' displayText='LinkedIn'></span><span  class='st_twitter_hcount' displayText='Tweet'></span><span  class='st_facebook_hcount' displayText='Facebook'></span>");
-////                $(this).append('<script type="text/javascript">var switchTo5x=true;</script><script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script><script type="text/javascript">stLight.options({publisher:\'4a2145e0-6211-42ee-b46f-2072b75cdd6a\'});</script>');
-//
-//                        // Facebook
-//                        $(this).append('<iframe src="http://www.facebook.com/plugins/like.php?href=' + _bf.host + '&amp;layout=button_count" scrolling="no" frameborder="0" style="border:none; width:90px; height:25px"></iframe>');
-//                    })
-//                    .appendTo($(this));
-//                    
-//                     $('<span />').attr(
-//                    {
-//                        'class': '_bf_social_bookmark'
-//                    })
-//                    .each(function()
-//                    {
-//                        // Twitter       
-//                        $(this).append('<script src="//platform.twitter.com/widgets.js" type="text/javascript"></script><div><a href="https://twitter.com/share?url=' + _bf.host + '" class="twitter-share-button">Tweet</a></div>');
-//                    })
-//                    .appendTo($(this));
-//
-               })
-               //.appendTo($(this));
-                
+                });                
 
                 for(i = 0; i < settings.share.length; i++)
                 {
@@ -121,14 +83,9 @@
                         .appendTo($(this))
                         .each(function()
                         {
-                            $('<img />').attr(
+                            $('<div />').attr(
                             {
-                                'class': '_bf_social_bookmark_image',
-                                alt: _bf.t('Social Share'),
-                                height: 25,
-                                width: 25,
-                                border: 0,
-                                src: settings.path + 'images/bare/' + settings.share[i].img
+                                'class': '_bf_social_bookmark_image _bf_social_bookmark_image_'+settings.share[i].media.toLowerCase()
                             })
                             .css(
                             {
