badeb3743033a8bbbb98d97e71b2ea3d5f08884b
Paul Whitrow <paul@pwhitrow.com>
2012-02-11 17:32:22 +0000

    admin updates

---
+++ admin/admin.js
@@ -0,0 +1,6 @@
+/* 
+    Document   : admin.js
+    Created on : 11-Feb-2012, 01:04:27
+    Author     : Paul Whitrow
+    Description: All admin Javacripts
+*/
--- admin/index.php
+++ admin/index.php
@@ -3,7 +3,7 @@
     Document   : index.php
     Created on : 10-Feb-2011, 14:46:22
     Author     : Paul Whitrow
-    Description: main index file for admin side
+    Description: Organisation signup/edit
 */
 
 require('./_bf_admin_include.php');
@@ -15,12 +15,49 @@ require('./_bf_admin_include.php');
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Client Signup</title>
+        <title>Organisation Signup</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+        <link href="css.css" rel="stylesheet">
+        
+        <script src="/api/libs/jquery.1.7.min.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            
+        $(document).ready(function() 
+        {
+            var error = false;
+            
+            $('#saveclient').click(function()
+            {
+                
+                $('.required').each(function()
+                {
+                    if($(this).val() == '')
+                    {
+                        var label = $(this).parent().find('label').html();
+                        error = true;
+                        alert('Oops, you missed ' + label);
+                        return false;
+                    }
+                })
+                .each(function()
+                {
+                    alert(error)
+                })
+                //$('.clientSignupForm').submit();
+            });
+        });
+            
+        </script>
+        
     </head>
     
     <body>
-        <h1>Client Signup</h1>
+        
+        <div class="header">
+            <h1><strong>Organisation</strong> <em>Signup</em></h1>
+        </div>
+        
         
         <div class="content">
             
@@ -29,15 +66,15 @@ require('./_bf_admin_include.php');
                 <ul class="formrows">
                     <li class="formrow">
                         <label for="name" class="formlabel">Name</label>
-                        <input type="text" name="clientname" id="name" class="formtextbox" value="" />
+                        <input type="text" name="clientname" id="name" class="formtextbox required" value="" />
                     </li>
                     <li class="formrow">
                         <label for="url" class="formlabel">URL</label>
-                        <input type="text" name="url" id="url" class="formtextbox" value="" />
+                        <input type="text" name="url" id="url" class="formtextbox required" value="" />
                     </li>
                     <li class="formrow">
                         <label for="description" class="formlabel">Description</label>
-                        <input type="text" name="description" id="description" class="formtextbox" value="" />
+                        <input type="text" name="description" id="description" class="formtextbox required" value="" />
                     </li>
                     <li class="formrow">
                         <label for="address" class="formlabel">Address</label>
@@ -45,31 +82,34 @@ require('./_bf_admin_include.php');
                     </li>
                     <li class="formrow">
                         <label for="tel" class="formlabel">Tel</label>
-                        <input type="text" name="tel" id="tel" class="formtextbox" value="" />
+                        <input type="text" name="tel" id="tel" class="formtextbox required" value="" />
                     </li>
                     <li class="formrow">
                         <label for="replytoemail" class="formlabel">Reply-to email</label>
-                        <input type="text" name="replytoemail" id="replytoemail" class="formtextbox" value="" />
-                    </li>
-                    <li class="formrow">
-                        <label for="reviews" class="formlabel">Reviews</label>
-                        <input type="checkbox" name="reviews" id="reviews" class="formtextbox" value="0" />
-                    </li>
-                    <li class="formrow">
-                        <label for="discussions" class="formlabel">Discussions</label>
-                        <input type="checkbox" name="discussions" id="discussions" class="formtextbox" value="0" />
-                    </li>
-                    <li class="formrow">
-                        <label for="watches" class="formlabel">Watches</label>
-                        <input type="checkbox" name="watches" id="watches" class="formtextbox" value="0" />
+                        <input type="text" name="replytoemail" id="replytoemail" class="formtextbox required" value="" />
                     </li>
                     <li class="formrow">
                         <label for="contactname" class="formlabel">Contact Name</label>
-                        <input type="text" name="contactname" id="contactname" class="formtextbox" value="" />
+                        <input type="text" name="contactname" id="contactname" class="formtextbox required" value="" />
                     </li>
                     <li class="formrow">
                         <label for="contactemail" class="formlabel">Contact Email</label>
-                        <input type="text" name="contactemail" id="contactemail" class="formtextbox" value="" />
+                        <input type="text" name="contactemail" id="contactemail" class="formtextbox required" value="" />
+                    </li>
+                    <li class="formrow">
+                        <label for="reviews" class="formlabel">Reviews</label>
+                        <input type="checkbox" name="reviews" id="reviews" class="formcheckbox" checked="checked" />
+                    </li>
+                    <li class="formrow">
+                        <label for="discussions" class="formlabel">Discussions</label>
+                        <input type="checkbox" name="discussions" id="discussions" class="formcheckbox" checked="checked" />
+                    </li>
+                    <li class="formrow">
+                        <label for="watches" class="formlabel">Watches</label>
+                        <input type="checkbox" name="watches" id="watches" class="formcheckbox" checked="checked" />
+                    </li>
+                    <li class="formrow">
+                        <input type="button" name="saveclient" id="saveclient" class="button" value="Save" />
                     </li>
                 </ul>    
                 
