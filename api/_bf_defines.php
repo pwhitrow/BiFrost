7a967e1c729a160b288f98c3b2b1d675647a8896
Matt Ayers <mr.matt.ayers@gmail.com>
2012-02-12 20:56:49 +0000

    Uploads fix and test site added.

--- api/_bf_defines.php
+++ api/_bf_defines.php
@@ -1,7 +1,7 @@
 <?php
 // Unchangeables!
 define('APP_NAME', 'BiFrost');
-define('MEDIA_LOCATION', 'uploads');
+define('MEDIA_LOCATION', '/uploads');
 define('DEV_EMAIL', 'paul@pwhitrow.com');
 define('TABLEPRENAME', '_bf_');
 
