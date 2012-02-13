7439cc0b8ade4568b55c740474309151df4d60db
Paul Whitrow <paul@pwhitrow.com>
2012-02-13 23:57:56 +0000

    rss update

--- api/rss.php
+++ api/rss.php
@@ -8,11 +8,11 @@ $org = "SELECT name, description, url FROM ".TABLEPRENAME."organisations WHERE a
 
 if($_REQUEST['type'] == 'reviews')
 {
-    $items = "SELECT id, title, content, DATE_FORMAT(posted,'%a, %e %b %Y %T') as fdate FROM ".TABLEPRENAME."reviews WHERE api_key='".$_REQUEST["apikey"]."' AND parent_id='".$_REQUEST["relation"]."' ORDER BY id DESC";
+    $items = "SELECT id, user_id, title, content, DATE_FORMAT(posted,'%a, %e %b %Y %T') as fdate FROM ".TABLEPRENAME."reviews WHERE api_key='".$_REQUEST["apikey"]."' AND parent_id='".$_REQUEST["relation"]."' ORDER BY id DESC";
 }
 if($_REQUEST['type'] == 'discussions')
 {
-    $items = "SELECT id, 'Comment' as title, content, DATE_FORMAT(posted,'%a, %e %b %Y %T') as fdate FROM ".TABLEPRENAME."discussions WHERE api_key='".$_REQUEST["apikey"]."' AND parent_id='".$_REQUEST["relation"]."' ORDER BY id DESC";
+    $items = "SELECT id, user_id, 'Comment' as title, content, DATE_FORMAT(posted,'%a, %e %b %Y %T') as fdate FROM ".TABLEPRENAME."discussions WHERE api_key='".$_REQUEST["apikey"]."' AND parent_id='".$_REQUEST["relation"]."' ORDER BY id DESC";
 }
 
 $org = mysql_query($org) or die(mysql_error());
@@ -29,11 +29,16 @@ echo '<?xml version="1.0" encoding="ISO-8859-1"?>
 
 while ($item = mysql_fetch_array($items))
 {
+    $sql = "SELECT fname, gname FROM ".TABLEPRENAME."users WHERE user_id = '".$item["user_id"]."'";
+    
+    $user = mysq_fetch_row(mysql_query($sql));
+    
     echo PHP_EOL.'<item>'.PHP_EOL.
-            '<title>'.cleanRSS($item[title]).'</title>'.PHP_EOL.
-            '<description><![CDATA['.cleanRSS($item[content]).']]></description>'.PHP_EOL.
-            '<guid>'.$org[url].'?reviewid='.$item[id].'</guid>'.PHP_EOL.
-            '<pubDate>'.$item[fdate].' GMT</pubDate>'.PHP_EOL.
+            '<title>'.cleanRSS($item["title"]).'</title>'.PHP_EOL.
+            '<description><![CDATA['.cleanRSS($item["content"]).']]></description>'.PHP_EOL.
+            '<author>Posted by <name>'.$user["gname"]." ".$user["fname"].'</name></author>'.PHP_EOL.
+            '<guid>'.$org["url"].'?reviewid='.$item["id"].'</guid>'.PHP_EOL.
+            '<pubDate>'.$item["fdate"].' GMT</pubDate>'.PHP_EOL.
             '</item>'.PHP_EOL.PHP_EOL;
 }
 echo '</channel>
