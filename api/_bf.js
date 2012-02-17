e90445b3cacf7afcb243585cf10197e3ef1be8dd
Paul Whitrow <paul@pwhitrow.com>
2012-02-17 23:41:39 +0000

    noconflict test

--- api/_bf.js
+++ api/_bf.js
@@ -13,6 +13,9 @@
 //var _bf_host = 'http://localhost:56870/';
 var _bf_host = 'http://bifrostplatform.co.uk/';
 
+var jQuery_bifrost = $.noConflict(true);	
+(function($) 
+{
 
 // insert a script
 function _bf_loadscript(src)
@@ -1946,3 +1949,4 @@ if(typeof $ != 'function')
 _bf_begin();
 
 
+})(jQuery_bifrost);
