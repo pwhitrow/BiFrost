25faa2198f452b3d3514e073af3a61767ffc9cb8
Paul Whitrow <paul@pwhitrow.com>
2012-02-20 08:41:02 +0000

    admin

--- admin/_bf_login.php
+++ admin/_bf_login.php
@@ -20,7 +20,7 @@ if($_REQUEST["action"] == "login")
     }
     
     $sql = "SELECT * FROM ".TABLEPRENAME."admins WHERE email='".$prep["username"]."' AND pass='".md5($prep["password"])."'";
-
+echo $sql;
     $sql = mysql_fetch_array($sql);
     
     if(mysql_num_rows($sql) > 0)
