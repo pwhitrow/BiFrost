665e402fbc36a6b89e3c0649824350c1f6becc25
Paul Whitrow <paul@pwhitrow.com>
2012-02-23 23:41:32 +0000

    test

--- api/_bf_api.php
+++ api/_bf_api.php
@@ -336,9 +336,7 @@ function remoteFileExists($url)
 
 function makeLinks($text) 
 {
-    $text = eregi_replace('(((f|ht){1}tp://)[-a-zA-Z0-9@:%_\+.~#?&//=]+)','<a href="\\1">\\1</a>', $text);
-    $text = eregi_replace('([[:space:]()[{}])(www.[-a-zA-Z0-9@:%_\+.~#?&//=]+)','\\1<a href="http://\\2">\\2</a>', $text);
-    $text = eregi_replace('([_\.0-9a-z-]+@([0-9a-z][0-9a-z-]+\.)+[a-z]{2,3})','<a href="mailto:\\1">\\1</a>', $text);
+    $text = ereg_replace("[[:alpha:]]+://[^<>[:space:]]+[[:alnum:]/]","<a href=\"\\0\">\\0</a>", $text);
     return $text;
 }
 
