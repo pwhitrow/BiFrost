ce0479ec6e939a541ac2768aeb93c9765c17a36f
Paul Whitrow <paul@pwhitrow.com>
2012-02-24 00:09:56 +0000

    tes

--- api/_bf.js
+++ api/_bf.js
@@ -1896,21 +1896,24 @@ function _bf_go()
                 return false;
             },
             
-            linkify: function(inputText, options) 
+            linkify: function(text) 
             {
-                this.options = {linkClass: 'url', targetBlank: true};
-                this.options = $.extend(this.options, options);
-                inputText = inputText.replace(/\u200B/g, "");
-                //URLs starting with http://, https://, or ftp://
-                var replacePattern1 = /(src="|href="|">|\s>)?(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;ï]*[-A-Z0-9+&@#\/%=~_|ï]/gim;
-                var replacedText = inputText.replace(replacePattern1, function($0,$1){return $1?$0:'<a class="'+ this.options.linkClass + '" href="' + $0 + '"' + (this.options.targetBlank?'target="_blank"':'') + '>'+ $0+ '</a>';});
-                //URLS starting with www and not the above
-                var replacePattern2 = /(src="|href="|">|\s>|https?:\/\/|ftp:\/\/)?www\.[-A-Z0-9+&@#\/%?=~_|!:,.;ï]*[-A-Z0-9+&@#\/%=~_|ï]/gim;
-                var replacedText = replacedText.replace(replacePattern2, function($0,$1){return $1?$0:'<a class="'+ this.options.linkClass + '" href="http://' + $0 + '"' + (this.options.targetBlank?'target="_blank"':'') + '>'+ $0+ '</a>';});
-                //Change email addresses to mailto:: links
-                var replacePattern3 = /([\.\w]+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim;
-                var replacedText = replacedText.replace(replacePattern3, '<a class="' + this.options.linkClass + '" href="mailto:$1">$1</a>');
-                return replacedText;
+		if( !text ) return text;
+		
+		text = text.replace(/((https?\:\/\/|ftp\:\/\/)|(www\.))(\S+)(\w{2,4})(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/gi,function(url){
+			nice = url;
+			if( url.match('^https?:\/\/') )
+			{
+				nice = nice.replace(/^https?:\/\//i,'')
+			}
+			else
+				url = 'http://'+url;
+			
+			
+			return '<a target="_blank" rel="nofollow" href="'+ url +'">'+ nice.replace(/^www./i,'') +'</a>';
+		});
+		
+		return text;
             },
 
             // cookies, yummy!
