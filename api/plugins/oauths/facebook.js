071f7674ec815a85827e2aa63b25d90ba91dc8f2
Paul Whitrow <paul@pwhitrow.com>
2012-02-27 23:32:12 +0000

    fb

--- api/plugins/oauths/facebook.js
+++ api/plugins/oauths/facebook.js
@@ -75,6 +75,7 @@ var fb_lib = {
         },
             function(response) 
             {
+console.log(response)
                 var response = response[0];
                 
                 var params = 
