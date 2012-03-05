d4117998a8cf13b7307aa75f0447f6a8420af4ff
Paul Whitrow <paul@pwhitrow.com>
2012-03-05 12:58:43 +0000

    avatar

--- api/views/changeavatar.js
+++ api/views/changeavatar.js
@@ -122,7 +122,7 @@ if(!$('._bf_' + formname + '_form').length)
         setNewAvatar: function(img)
         {
             // update dashboard image
-            $('._bf_dashboard_avatar').attr('src', img);
+            $('._bf_dashboard_avatar').attr('src', _bf.host + img);
 
             // post to api
             _bf.post(
